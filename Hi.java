9ffb69e57575fbd7783489eec842ef7062e03be1
Mithilesh09 <58629324+Mithilesh09@users.noreply.github.com>
2019-12-19 12:01:04 +0530

    Delete Hi.java

--- Hi.java
+++
@@ -1,8 +0,0 @@
-public class Hi {
-
-    public static void main(String[] args) {
-        // Prints "Hello, World" to the terminal window.
-        System.out.println("Hello, MIT it's java from github");
-    }
-
-}
